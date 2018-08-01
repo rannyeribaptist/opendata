6f29d48fba5e568a2c14ea2e0010423bcaf0178d
rannyeribaptist <rannyerirbatista@gmail.com>
2018-08-01 13:58:23 -0300

    :rocket: First commit with initial project structure and first section created with all the breakpoints

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  $('.presentation-carousel').slick({
+    prevArrow:"<i class='icon-arrow-left' style='font-size:30px; position:absolute; left: -95px; color: #296460; top:40%';></i>",
+    nextArrow:"<i class='icon-arrow-right' style='font-size:30px; position:absolute; right: -95px; top: 40%; color: #296460'></i>"
+  });
+});
