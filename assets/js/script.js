51eef7213147a4052bd105211a06c50589b2644e
rannyeribaptist <rannyerirbatista@gmail.com>
2018-08-15 18:01:28 -0300

    :construction: map section, but missing responsive and code review

--- assets/js/script.js
+++ assets/js/script.js
@@ -187,3 +187,160 @@ drawChart([{'date': 2014, 'value': 0}, {'date': 2015, 'value': 10}, {'date': 201
 $('nav').on('click', function() {
 	$('.column').fadeToggle(300);
 })
+
+var areas = [];
+areas[0] = [];
+// Africa
+areas[1] = ["AO", "GA", "NG", "BJ", "GM", "RW", "BW",
+						"GH", "BF", "GW", "SN", "BI", "KE", "SL",
+						"CM", "LS", "SO", "CF", "LR", "ZA", "TD",
+						"MG", "SS", "CD", "MR", "TG", "GQ", "MZ",
+						"UG", "ER", "NA", "ZM", "ET", "NE", "ZW", "Africa"];
+// Latin America
+areas[2] = ["PY", "AR", "PE", "DO", "PR", "BS", "EC",
+						"SV", "BZ", "BR", "GY", "SR", "HT", "HN",
+						"TT", "CL", "JM", "CO", "MX", "UY", "CR",
+						"NI", "VE", "CU", "PA", "BO", "LatinAmerica"];
+// Asia
+areas[3] = ["AF", "IN", "PK", "BD", "LK", "BT", "NP", "Asia"];
+// Carribean
+areas[4] = ["BS", "CU", "GB", "HT", "DO", "JM", "PR", "Carribean"];
+// Eastern Europe & Central Asia
+areas[5] = ["AL", "AM", "AZ", "BY", "BA", "GE", "KZ",
+						"XK", "KG", "MK", "MD", "ME", "RU", "RS",
+						"TJ", "TM", "UA", "UZ", "ECA"];
+// Middle East & North Africa
+areas[6] = ["DZ", "JO", "QA", "KW", "SA", "DJ", "LB",
+						"EG", "SY", "LY", "TN", "IR", "AE", "IQ",
+						"MA", "IL", "OM", "YE", "MiddleEast"];
+
+function selectArea(code){
+	var mapObj = $("#map").vectorMap("get", "mapObject");
+	areas.forEach(function(area) {
+		if(area.indexOf(code)>-1) {
+			$('#'+area[area.length-1]).addClass('active');
+			$('.map').find('.button'+area[area.length-1]).addClass("button-active");
+			mapObj.setSelectedRegions(area.slice(0, area.length-1));
+			return;
+		}
+	});
+}
+function clearAll(){
+	var mapObj = $("#map").vectorMap("get", "mapObject");
+	mapObj.clearSelectedRegions();
+
+	$('.map').find('.info.active').removeClass('active');
+	$('.map').find('.button-active').removeClass('button-active');
+}
+
+$('#map').vectorMap({
+	map: 'world_merc',
+	zoomButtons: false,
+	backgroundColor: 'transparent',
+	zoomOnScroll: false,
+	zoomMin: 0.7,
+	focusOn: {
+		x: 0.0,
+		y: 0.0,
+		scale: 0.7
+	},
+	markerStyle: {
+		initial: {
+			fill: '#F8E23B',
+			stroke: '#383f47'
+		}
+	},
+	markers: [
+		{ latLng: [12, -72.45], name: 'Education', style: {r:9, fill:'#ec5563', class:'jvectormap-marker jvectormap-element education'} },
+		{ latLng: [-20, -60], name: 'Education', style: {r:9, fill:'#ec5563', class:'jvectormap-marker jvectormap-element education'} },
+		{ latLng: [-30, -55], name: 'Education', style: {r:9, fill:'#ec5563', class:'jvectormap-marker jvectormap-element education'} },
+		{ latLng: [-05, 20], name: 'Education', style: {r: 10, fill:'#ec5563', class:'jvectormap-marker jvectormap-element education'} },
+		{ latLng: [0, 28], name: 'Education', style: {r: 10, fill:'#ec5563', class:'jvectormap-marker jvectormap-element education'} },
+		{ latLng: [-15, 24], name: 'Education', style: {r: 15, fill:'#ec5563', class:'jvectormap-marker jvectormap-element education'} },
+		{ latLng: [32, 70], name: 'Education', style: {r: 10, fill:'#ec5563', class:'jvectormap-marker jvectormap-element education'} },
+		{ latLng: [29, 90], name: 'Education', style: {r: 10, fill:'#ec5563', class:'jvectormap-marker jvectormap-element education'} },
+		{ latLng: [9, 115], name: 'Education', style: {r: 10, fill:'#ec5563', class:'jvectormap-marker jvectormap-element education'} },
+		{ latLng: [14, 122], name: 'Education', style: {r: 10, fill:'#ec5563', class:'jvectormap-marker jvectormap-element education'} },
+		{ latLng: [3, 127], name: 'Education', style: {r: 10, fill:'#ec5563', class:'jvectormap-marker jvectormap-element education'} },
+
+		{ latLng: [22, -70], name: 'Agriculture', style: {r: 15, fill:'#41aef0', class:'jvectormap-marker jvectormap-element agriculture'} },
+		{ latLng: [-22, -48], name: 'Agriculture', style: {r: 15, fill:'#41aef0', class:'jvectormap-marker jvectormap-element agriculture'} },
+		{ latLng: [14, 133], name: 'Agriculture', style: {r: 15, fill:'#41aef0', class:'jvectormap-marker jvectormap-element agriculture'} },
+
+		{ latLng: [16.5, -72], name: 'Gender', style: {r: 5, fill:'#f7b534', class:'jvectormap-marker jvectormap-element gender'} },
+		{ latLng: [-25, -60], name: 'Gender', style: {r: 5, fill:'#f7b534', class:'jvectormap-marker jvectormap-element gender'} },
+		{ latLng: [-26, -55], name: 'Gender', style: {r: 5, fill:'#f7b534', class:'jvectormap-marker jvectormap-element gender'} },
+		{ latLng: [-7, 27], name: 'Gender', style: {r: 5, fill:'#f7b534', class:'jvectormap-marker jvectormap-element gender'} },
+		{ latLng: [37, 72], name: 'Gender', style: {r: 5, fill:'#f7b534', class:'jvectormap-marker jvectormap-element gender'} },
+		{ latLng: [34, 93], name: 'Gender', style: {r: 5, fill:'#f7b534', class:'jvectormap-marker jvectormap-element gender'} },
+		{ latLng: [8, 121], name: 'Gender', style: {r: 5, fill:'#f7b534', class:'jvectormap-marker jvectormap-element gender'} },
+		{ latLng: [8, 126], name: 'Gender', style: {r: 5, fill:'#f7b534', class:'jvectormap-marker jvectormap-element gender'} },
+
+		{ latLng: [15, -68], name: 'Anti-corruption', style: {r:8, fill:'#1e5468', class:'jvectormap-marker jvectormap-element anti-corruption'} },
+		{ latLng: [-30, -48], name: 'Anti-corruption', style: {r:8, fill:'#1e5468', class:'jvectormap-marker jvectormap-element anti-corruption'} },
+		{ latLng: [32, 60], name: 'Anti-corruption', style: {r: 15, fill:'#1e5468', class:'jvectormap-marker jvectormap-element anti-corruption'} },
+		{ latLng: [28, 80], name: 'Anti-corruption', style: {r: 15, fill:'#1e5468', class:'jvectormap-marker jvectormap-element anti-corruption'} },
+		{ latLng: [37, 79], name: 'Anti-corruption', style: {r: 10, fill:'#1e5468', class:'jvectormap-marker jvectormap-element anti-corruption'} },
+		{ latLng: [32, 99], name: 'Anti-corruption', style: {r: 10, fill:'#1e5468', class:'jvectormap-marker jvectormap-element anti-corruption'} },
+		{ latLng: [-1, 117], name: 'Anti-corruption', style: {r: 15, fill:'#1e5468', class:'jvectormap-marker jvectormap-element anti-corruption'} },
+		{ latLng: [4.5, 135], name: 'Anti-corruption', style: {r: 10, fill:'#1e5468', class:'jvectormap-marker jvectormap-element anti-corruption'} },
+
+		{ latLng: [-23, -56], name: 'Cities', style: {r:3, fill:'#f07125', class:'jvectormap-marker jvectormap-element cities'} },
+		{ latLng: [-05, 30], name: 'Cities', style: {r:3, fill:'#f07125', class:'jvectormap-marker jvectormap-element cities'} },
+		{ latLng: [37, 68], name: 'Cities', style: {r:3, fill:'#f07125', class:'jvectormap-marker jvectormap-element cities'} },
+		{ latLng: [33.7, 88], name: 'Cities', style: {r:3, fill:'#f07125', class:'jvectormap-marker jvectormap-element cities'} },
+		{ latLng: [11, 125], name: 'Cities', style: {r:3, fill:'#f07125', class:'jvectormap-marker jvectormap-element cities'} },
+		{ latLng: [4.5, 122], name: 'Cities', style: {r:3, fill:'#f07125', class:'jvectormap-marker jvectormap-element cities'} },
+	],
+	regionStyle: {
+		initial: {
+			fill: '#31746cbf'
+		},
+		hover: {
+			fill: "#29655e"
+		},
+		selected: {
+			fill: "#235754"
+		}
+	},
+	onRegionClick: function(e, code){
+		clearAll();
+		selectArea(code);
+		return false;
+	}
+});
+
+$('#map').find('button').on('click', function() {
+	$('#map').find('.button-active').removeClass('button-active');
+	$(this).addClass('button-active');
+	clearAll();
+	selectArea($(this).attr('data-code'));
+});
+
+$('#map').find('h1').on('click', function() {
+	$('#map').find('.title-active').removeClass('title-active');
+	$('#map').find('.buttons-menu').removeClass('buttons-active');
+	$(this).addClass('title-active');
+	clearAll();
+
+	var trigger = $(this);
+
+	if(trigger.attr('data-value') === 'regions') {
+		$('#map').find('.map-buttons').addClass('buttons-active');
+		$('.jvectormap-marker').hide();
+		$('#map').find('.info').removeClass('hide');
+	} else {
+		$('#map').find('.map-buttons-filters').addClass('buttons-active');
+		$('.jvectormap-marker').show();
+		$('#map').find('.info').addClass('hide');
+	}
+});
+
+$('.filter').on('click', function() {
+	$('.jvectormap-marker').hide();
+	$('.'+$(this).attr('data-code')).show();
+
+	if($(this).attr('data-code') === 'all'){
+		$('.jvectormap-marker').show();
+	}
+});
